f071c57b9fd84fe8d89af32670d112016bb16e0d
KunRalf <rshumaher1996@gmail.com>
2021-09-22 04:29:52 +0300

    add movement ot player

---
+++ GunChero/Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] private FloatingJoystick _joystick;
+    private Animator _animator;
+
+    private const string RUNNIG_FLAG = "_isRun";
+    private const string ATTACKING_TRIGGER = "Attack";
+
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void RunningAnimation()
+    {
+        if (_joystick.Horizontal > 0 || _joystick.Vertical > 0)
+        {
+            _animator.SetBool(RUNNIG_FLAG, true);
+        }
+
+        if (_joystick.Horizontal == 0 || _joystick.Vertical == 0)
+        {
+            _animator.SetBool(RUNNIG_FLAG, false);
+        }
+    }
+
+    private void Update()
+    {
+        RunningAnimation();
+    }
+}
---
+++ GunChero/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _gravity = 0.5f;
+    [SerializeField] private FloatingJoystick _joystick;
+
+    private Vector3 _playerDirection;
+    private float _rotateSpeed = 0.1f;
+    private Rigidbody _playerRgb;
+
+
+
+    private void Start()
+    {
+        _playerRgb = GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        _playerDirection.y = 0;
+        _playerDirection.x = _joystick.Horizontal;
+        _playerDirection.z = _joystick.Vertical;
+
+        if (_playerDirection != Vector3.zero)
+        {
+            Vector3 moveDirection = Vector3.RotateTowards(transform.forward, _playerDirection, _rotateSpeed, 0f);
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+
+        }
+        _playerRgb.velocity = new Vector3(_playerDirection.normalized.x * _moveSpeed, _playerRgb.velocity.y - _gravity, _playerDirection.normalized.z * _moveSpeed);
+    }
+}
